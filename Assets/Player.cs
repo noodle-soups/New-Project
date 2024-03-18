ea3818d683c95d25a2ce185fce8caf59fa986ea1
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-03-18 18:20:32 +0000

    new movePlayer built

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,25 +7,47 @@ public class Player : MonoBehaviour
 
     Rigidbody2D rb;
 
+    // movePlayer
     float inputHorizontal;
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeedOld = 10f;
 
-    // Start is called before the first frame update
+    float moveInput;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float acceleration = 7f;
+    [SerializeField] float decceleration = 7f;
+    [SerializeField] float velPower = 0.9f;
+ 
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();   
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        movePlayer();
+        //movePlayer();
+        newMovePlayer();
     }
 
     void movePlayer()
     {
-        inputHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        inputHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeedOld;
         rb.AddForce(inputHorizontal * Vector2.right);     
+    }
+
+    void newMovePlayer()
+    {
+        // 
+        moveInput = Input.GetAxisRaw("Horizontal");
+        float targetSpeed = moveInput * moveSpeed;
+        float speedDif = targetSpeed - rb.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+
+        //rb.AddForce(targetSpeed * Vector2.right);
+        rb.AddForce(movement * Vector2.right);
     }
 
 }
